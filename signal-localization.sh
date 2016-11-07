d6cafe5bb5c606aa9a455490c3f014c877a8e953
Alexandros Armaos <alexandrosarmaos@dgkqr098gf1l.crg.es>
2016-11-07 18:43:26 +0100

    new model

--- signal-localization.sh
+++ signal-localization.sh
@@ -18,13 +18,11 @@ for i in `awk '{print $0}' $6`;do
 
   ( cp -r template tmp/$1/$i
 
-  cp eclip_selected_transcripts/$i.rna.fragm.seq.rna.lib tmp/$1/$i/interactions.U/combine_parallel/rna/
-#  cp lincrnas/$i.rna.fragm.seq.rna.lib tmp/$1/$i/interactions.U/combine_parallel/rna/
 
   cp lincrnas/$i.rna.fragm.seq.rna.lib tmp/$1/$i/interactions.U/combine_parallel/rna/
 	cp $5 tmp/$1/$i/outputs/protfile
 	prot_file=$(readlink -f tmp/$1/$i/outputs/protfile)
-	
+
   cd   tmp/$1/$i
 	bash run_signal_localization_case.sh $1 $2 $3 $4 $prot_file $6 $script_folder $i
 	cd ../../../ ) &
--- template/filter/start.sh
+++ template/filter/start.sh
@@ -27,7 +27,7 @@ sh scripts/run.sh new run > tmp/input.2.tmp
 f="params"
 
 
-awk '(NF>2){s=0; for(i=2;i<=NF-1;i++){s=s+$i; a[i]=$i} for(i=2;i<=NF-1;i++){if(s==0){v=a[i]} if(s!=0){v=a[i]/s} printf "%s\t", v} printf "%s\t",$NF; printf "\n"} (NF==2){print $2}' tmp/input.2.tmp > tmp/input.txt
+awk '(NF>2){s=0; for(i=2;i<=NF;i++){s=s+$i; a[i]=$i} for(i=2;i<=NF;i++){if(s==0){v=a[i]} if(s!=0){v=a[i]/s} printf "%s\t", v} printf "%s\t",$NF; printf "\n"} (NF==2){print $2}' tmp/input.2.tmp > tmp/input.txt
 
 cat $f/min.relative.i.txt $f/max.relative.i.txt  $f/min.relative.o.txt $f/max.relative.o.txt tmp/input.txt | awk '(NR==1){for(i=1;i<=NF;i++){m[i]=$i}} (NR==2){for(i=1;i<=NF;i++){M[i]=$i}} (NR==3)&&(NF==1){m2[1]=$1} (NR==4)&&(NF==1){M2[1]=$1} (NR>4)&&(NF>1){for(i=1;i<=NF;i++){ s=0; if (m[i]!=M[i]){s=(($i-m[i])/(M[i]-m[i])-1/2)*2;} printf "%4.2f\t",s} printf "\n"} (NR>4)&&(NF==1){s=0; if (m2[1]!=M2[1]){s=(($1-m2[1])/(M2[1]-m2[1])-1/2)*2} print s}'  > tmp/norm.tmp
 
@@ -37,5 +37,5 @@ cat $f/min.relative.i.txt $f/max.relative.i.txt  $f/min.relative.o.txt $f/max.re
 awk '{a[NR]=$0} (NF!=1){s=NF} END{printf "%i\t%i\t%i\n",NR/2,s, 1; for(i=1;i<=NR;i++){printf "%s\n",a[i]}}' tmp/norm.tmp  > input.txt
 
 
-./scripts/test input.txt ./models/secondary.130 | awk '(NF==3)' > tmp/output.txt
+./scripts/test input.txt ./models/secondary.50 | awk '(NF==3)' > tmp/output.txt
 paste tmp/names.txt tmp/output.txt | awk '{print $1, $NF}'
