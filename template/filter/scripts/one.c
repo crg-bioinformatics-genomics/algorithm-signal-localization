debb4adde01fc8070ad43134e8b0ab2c8fb0913f
Alexandros Armaos <alexandrosarmaos@dgkqr098gf1l.crg.es>
2016-11-29 17:56:55 +0100

    sl_nnetwork 3commit compile

--- template/filter/scripts/one.c
+++ template/filter/scripts/one.c
@@ -27,24 +27,24 @@ int main(int argc, char ** argv)
 
 	struct fann *ann = fann_create_from_file(argv[2]);
         struct fann_train_data *data = fann_read_train_from_file(argv[1]);
-	
+
 	unsigned int i = 0;
 	int j = 0;
 	unsigned int decimal_point;
 	for(i = 0; i < data->num_data; i++){
 		calc_out = fann_run(ann, data->input[i]);
 		printf("%i\n",i+1);
-		for(j=0;j<1;j++)
+		for(j=0;j<100;j++)
 		{
 		  printf(" %i %f  %f\n",
 			 j+1,
 			 data->output[i][j],
 			 calc_out[j]);
-			 
+
 		}
 
 	}
-	
+
 	fann_destroy(ann);
 	return 0;
 }
