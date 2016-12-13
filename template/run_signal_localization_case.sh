18602ebec1de2cfac5727363587dfb7ddfced379
Alexandros Armaos <alexandrosarmaos@dgkqr098gf1l.crg.es>
2016-12-13 19:25:54 +0100

    plotter.r

--- template/run_signal_localization_case.sh
+++ template/run_signal_localization_case.sh
@@ -82,7 +82,9 @@ cd filter
 
 
 cd ../
-  awk '(NR>1)&&($3!="nan")' ./outputs/$case.fragments.score.txt | sort -k3nr | awk 'BEGIN{printf "<tbody>\n"}{if ($3 >=0.5) class="green" ; else class="red" fi; printf "\t<tr class=\"%s\">\n\t\t<td>%s</td>\n\t\t<td>%s</td>\n\t\t<td>%.3f</td>\n\t</tr>\n", class,NR,$1"-"$2, $3}END{print """</tbody>"""}' > ./outputs/$case.binding_sites.html
+  awk '(NR>1)&&($3!="nan")' ./outputs/$case.fragments.score.txt | sort -k3nr | awk 'BEGIN{printf "<tbody>\n"}{if ($3 >=0.25) class="red" ; else class="white" fi; printf "\t<tr class=\"%s\">\n\t\t<td>%s</td>\n\t\t<td>%s</td>\n\t\t<td>%.3f</td>\n\t</tr>\n", class,NR,$1"-"$2, $3}END{print """</tbody>"""}' > ./outputs/$case.binding_sites.html
+  awk '($3!="nan"){print $0}' ./outputs/$case.fragments.score.txt >tmp.1
+  mv tmp.1 ./outputs/$case.fragments.score.txt
   Rscript plotter.r ./outputs/$case.fragments.score.txt
   convert -density 300 -trim binding_sites.pdf -quality 100 -resize 900x231 binding_sites.png
   mv binding_sites.png ./outputs/$case.binding_sites.png
